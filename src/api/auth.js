5ade3528509ae216606b95122fab993530dc0fcb
zhangqian6 <zhangdada462@gmail.com>
2025-07-27 15:52:31 +0800

    增加前端用户管理页面中添加用户页面功能

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Flask后端API地址
+  baseURL: 'http://localhost:5000/api', // Flask后端API地址
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
--- src/views/UserManage.vue
+++ src/views/UserManage.vue
@@ -1,6 +1,36 @@
 <template>
     <div class="usermanage">
-        <div class="batch_del">
+        <div class="action_bar">
+            <el-button type="primary" @click="dialogVisible=true">添加读者</el-button>
+            <el-dialog
+                v-model="dialogVisible"
+                title="添加读者"
+            >
+                <el-form :model="form" label-width="auto">
+                    <el-form-item label="读者编号" prop="id">
+                        <el-input v-model="form.id" placeholder="请输入读者编号" />
+                    </el-form-item>
+                    <el-form-item label="用户名" prop="id">
+                        <el-input v-model="form.id" placeholder="请输入用户名" />
+                    </el-form-item>
+                    <el-form-item label="姓名" prop="id">
+                        <el-input v-model="form.id" placeholder="请输入姓名" />
+                    </el-form-item>
+                    <el-form-item label="电话号码" prop="id">
+                        <el-input v-model="form.id" placeholder="请输入电话号码" />
+                    </el-form-item>
+                    <el-form-item label="性别" prop="id">
+                        <el-input v-model="form.id" placeholder="请输入性别" />
+                    </el-form-item>
+                    <el-form-item label="地址" prop="id">
+                        <el-input v-model="form.id" placeholder="请输入地址" />
+                    </el-form-item>
+                </el-form>
+                <template #footer>
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="submitForm">确认添加</el-button>
+                </template>
+            </el-dialog>
             <el-button type="danger" @click="batchDelete">批量删除</el-button>
         </div>
         <el-table :data="[]" border style="width: 100%">
@@ -12,7 +42,7 @@
             <el-table-column prop="address" label="地址" />
             <el-table-column prop="action" label="操作">
                 <template #default="scope">
-                    <el-button type="primary" size="small" @click="handleEdit(scope.row)">修改</el-button>
+                    <el-button type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
                     <el-button type="danger" size="small" @click="openDeleteDialog(scope.row)">删除</el-button>
 
                 </template>
@@ -27,6 +57,24 @@
 </template>
 
 <script setup>
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { onMounted, ref, watch } from 'vue';
+import axios from 'axios';
+
+const form = ref({
+    id: '',
+    user: '',
+    name: '',
+    phone_number: '',
+    gender: '',
+    address: ''
+});
+const dialogVisible = ref(false);
+
+
+function submitForm(){
+    console.log("添加读者功能尚未实现");
+}
 function batchDelete() {
     // 这里可以添加批量删除的逻辑
     console.log("批量删除功能尚未实现");
